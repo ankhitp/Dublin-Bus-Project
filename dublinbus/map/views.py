c8049ccbb3be1c118e9a02382eb2ebbea0fc7d80
enxicui <enxi.cui@gmail.com>
2019-06-24 16:49:01 +0100

    markers

--- dublinbus/map/views.py
+++ dublinbus/map/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+
+# Create your views here.
+
+import json
 # Create your views here.
 
 def index(request):
+    file = open("/Users/judy/PycharmProjects/dublinbus/static/files/stops_info.json","r")
     # return HttpResponse(render({}, request))
-    return render(request, "index.html", {})
+    loadjson = json.load(file)
+    file.close()
 
+    return render(request, "index.html", {
+        'load': loadjson,
+    })
