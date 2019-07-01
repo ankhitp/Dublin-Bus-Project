310aee692d8eb2d465548000b46fefc691db8800
enxicui <enxi.cui@ucdconnect.ie>
2019-07-01 16:29:55 +0100

    add infowindow function

--- dublinbus/map/views.py
+++ dublinbus/map/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from .models import *
 
 # Create your views here.
 
@@ -8,7 +9,7 @@ import json
 # Create your views here.
 
 def index(request):
-    file = open("/Users/judy/PycharmProjects/dublinbus/static/files/stops_info.json","r")
+    file = open("/Users/judy/Desktop/dublinbus/dublinbus/static/files/stops_info.json","r")
     # return HttpResponse(render({}, request))
     loadjson = json.load(file)
     file.close()
