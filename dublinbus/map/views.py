59e22eda6f728d78e03f79619ef4804134c104a7
Ankhit <aapandurangi@ucsd.edu>
2019-07-01 14:12:09 +0100

    new stuff for routing

--- dublinbus/map/views.py
+++ dublinbus/map/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import JsonResponse
 from django.template import loader
+from datetime import datetime
+import os
+import requests
+from django.views.decorators.csrf import csrf_exempt
+dirname = os.path.dirname(__file__)
+filename = os.path.join(dirname, "../static/files/stops_info.json")
 
 # Create your views here.
 
@@ -8,7 +15,7 @@ import json
 # Create your views here.
 
 def index(request):
-    file = open("/Users/judy/PycharmProjects/dublinbus/static/files/stops_info.json","r")
+    file = open(filename,"r")
     # return HttpResponse(render({}, request))
     loadjson = json.load(file)
     file.close()
@@ -16,3 +23,21 @@ def index(request):
     return render(request, "index.html", {
         'load': loadjson,
     })
+
+@csrf_exempt
+def getRoutes(request):
+    if 'startLat' in request.POST:
+        startLat = request.POST['startLat']
+    if 'startLong' in request.POST:
+        startLong = request.POST['startLong']
+    if 'destLat' in request.POST:
+        destLat = request.POST['destLat']
+    if 'destLong' in request.POST:
+        destLong = request.POST['destLong']
+    myTime = datetime.now().isoformat()
+    url = "https://transit.api.here.com/v3/route.json?app_id=tL7r9QKJ3KlE5Kc9LGYo&app_code=1arMcSHt_o31xFSeBRswsA&modes=bus&routing=all&dep="+startLat+","+startLong+"&arr="+destLat+","+destLong+"&time="+str(myTime)
+    print(url)
+    response = requests.get(url)
+    response = response.json()
+    return JsonResponse(response,safe=False)
+
