fda30e15f300da8a578d37b1f5c3127f6282f253
byrnec30 <clare.byrne@ucdconnect.ie>
2019-07-03 15:19:29 +0100

    changed views.py

--- dublinbus/journeyplan/views.py
+++ dublinbus/journeyplan/views.py
@@ -5,4 +5,12 @@ from django.template import loader
 
 def journeyplan(request):
     # return HttpResponse(render({}, request))
-    return render(request, "journeyplan.html", {})
+    json_data = open('static/files/stops_info.json')
+    stops_data = json.load(json_data)
+    user_agent = get_user_agent(request)
+    if user_agent.is_mobile:
+        return render(request, 'mobile/m_journeyplan.html', {'load': stops_data})
+    elif user_agent.is_tablet:
+        return render(request, 'mobile/m_journeyplan.html', {'load': stops_data})
+    else:
+        return render(request, 'journeyplan.html', {'load': stops_data})
