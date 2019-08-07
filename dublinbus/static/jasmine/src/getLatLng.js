ae7d506dfb6fb116c107a4bfdba4163471583a07
byrnec30 <clare.byrne@ucdconnect.ie>
2019-08-07 12:57:18 +0100

    newjasmine started

---
+++ dublinbus/static/jasmine/src/getLatLng.js
@@ -0,0 +1,61 @@
+/**
+ * The getLatLng() function is the first one called in the setting up of the directions window. The function gets the start
+ * and end points off of the input boxes, and then calls the Google Geocode Service to get latitude and longitudes for each
+ * spot. From there, it calls the HERE Transit API, which will provide a list of routes and directions between the
+ * two latitude and longitude points. The function then tells the user which bus lines are possible between two points
+ * and also indicates how many connections each route has.
+ */
+function setupLatLng(start, end) {
+    document.getElementById('options').style.height = "26vh";
+    document.getElementById('options').innerHTML = "<h3>Possible Routes</h3>";
+
+    start = start.replace("'", "\\'");
+    //set the HTML for the routes list
+    //start and end points
+    var dublin = {
+        lat: 53.33306,
+        lng: -6.24889
+    };
+    map.panTo(dublin);
+    getLatLng(start, end)
+}
+
+function getLatLng(start, end) {
+    //Two geocoders, one for the start and one for end
+    var geocoder = new google.maps.Geocoder();
+    var geocoder2 = new google.maps.Geocoder();
+    var url = "";
+    geocoder.geocode({
+        'address': start
+    }, function(results, status) {
+        if (status == 'OK') {
+            var loc = results[0].geometry.location;
+            console.log("loc",loc);
+            startLat = loc.lat();
+            startLong = loc.lng();
+        }
+        geocoder2.geocode({
+            'address': end
+        }, function(results, status) {
+            if (status == 'OK') {
+                //locations are stored in a variable called results. results[0] is the most accurate one, the list
+                //results get less accurate as you go down, so if you looked at results[1], it'll be less specific
+                var loc = results[0].geometry.location;
+                destLat = loc.lat();
+                destLong = loc.lng();
+                //new xhttp request to get the data from the HERE api
+                xhttp = new XMLHttpRequest();
+                var date = new Date();
+                date = date.toISOString();
+                url = "http://transit.api.here.com/v3/route.json?app_id=tL7r9QKJ3KlE5Kc9LGYo&app_code=1arMc" +
+                    "SHt_o31xFSeBRswsA&modes=bus&routing=all&dep=" + startLat + "," + startLong + "&arr=" + destLat +
+                    "," + destLong + "&time=" + date;
+                sendRequest(url, start, end);
+
+            }
+        })
+
+    })
+    return geocoder2.geocode.address;
+}
+
---
+++ dublinbus/static/jasmine/src/sendRequest.js
@@ -0,0 +1,26 @@
+
+
+
+function sendRequest(url, start, end) {
+    console.log("clare it worked")
+    var url = url;
+    var start = start;
+    var end = end;
+    xhttp.open("GET", url, true);
+    xhttp.setRequestHeader('content-type', 'application/x-www-form-urlencoded;charset=UTF-8');
+    //xhttp.setRequestHeader('X-CSRF-Token', 'abcdef');
+    xhttp.send();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState === 4 && this.status === 200) {
+            console.log(url);
+            //parsing this awful JSON. follow the url if you want to see how the JSON looks
+            var returnData = JSON.parse(this.responseText);
+            var parseMe = returnData['Res']['Connections']["Connection"];
+            printResults(parseMe, url, start, end);
+
+        }
+    }
+}
+
+
+
