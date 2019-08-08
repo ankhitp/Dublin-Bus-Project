bc1067ada9c1b3eafa252f98aad116d47fd99148
enxicui <enxi.cui@ucdconnect.ie>
2019-08-08 13:00:25 +0100

    journey view

--- dublinbus/journeyplan/views.py
+++ dublinbus/journeyplan/views.py
@@ -29,14 +29,23 @@ def journeyplan(request):
 
 
 def bus_prediction(request):
-    input = {'direction': 1, 'dayOfWeek': '2', 'rushHour': 1, 'monToThurRushHour': 1, 'friday': 0, 'windSpeed': 3, 'temp': 5}
+    direction = request.POST.get("direction")
+    dayOfWeek = request.POST.get("dayOfWeek")
+    rushHour = request.POST.get("rushHour")
+    monToThurRushHour = request.POST.get("direction")
+    friday = request.POST.get("direction")
+    windSpeed = request.POST.get("windSpeed")
+    temp = request.POST.get("temp")
     randomForest_Results = {}
 
-    with open("static/pickle/15A_1_pickle.pkl", "rb") as handle:
+    with open("static/pickle/15A_1_pickle", "rb") as handle:
         model = pickle.load(handle)
-        print(model)
-        a = model.predict(input)
-
-
+        totaljourney = 0
+        for key, value in model.items():
+            thismodel = model[key]
+            thisprediction = thismodel.predict(numpy.array([[direction, dayOfWeek, rushHour, monToThurRushHour, friday, windSpeed, temp]]))
+            randomForest_Results[key] = thisprediction
+            totaljourney += thisprediction
+    print("total journey time: ", totaljourney / 60, "minutes")
     print(randomForest_Results)
-    return HttpResponse("123")
+    return JsonResponse(randomForest_Results)
