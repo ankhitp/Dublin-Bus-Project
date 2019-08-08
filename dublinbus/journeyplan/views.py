85f0b3120e422b35bb629f32987ab4e24c310b54
enxicui <enxi.cui@ucdconnect.ie>
2019-08-08 10:49:23 +0100

    change co2

--- dublinbus/journeyplan/views.py
+++ dublinbus/journeyplan/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,JsonResponse
 from django.template import loader
 import json
 from django_user_agents.utils import get_user_agent
@@ -7,6 +7,13 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 import json
+import pickle
+import requests
+import sqlalchemy
+import requests
+import time
+import numpy
+
 
 def journeyplan(request):
     # return HttpResponse(render({}, request))
@@ -19,3 +26,17 @@ def journeyplan(request):
         return render(request, 'mobile/m1_journeyplan.html', {'load': stops_data})
     else:
         return render(request, 'journeyplan.html', {'load': stops_data})
+
+
+def bus_prediction(request):
+    input = {'direction': 1, 'dayOfWeek': '2', 'rushHour': 1, 'monToThurRushHour': 1, 'friday': 0, 'windSpeed': 3, 'temp': 5}
+    randomForest_Results = {}
+
+    with open("static/pickle/15A_1_pickle.pkl", "rb") as handle:
+        model = pickle.load(handle)
+        print(model)
+        a = model.predict(input)
+
+
+    print(randomForest_Results)
+    return HttpResponse("123")
