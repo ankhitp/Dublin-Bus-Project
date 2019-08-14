6115a833c4ecebe501b07cded6cd1fefaa84c1cf
Ankhit <aapandurangi@ucsd.edu>
2019-08-14 11:47:08 +0100

    fixing lots of thigns

--- dublinbus/static/js/createfavourite.js
+++ dublinbus/static/js/createfavourite.js
@@ -15,7 +15,7 @@ document.getElementById("addfavourite").addEventListener("click", function () {
     //    "</div>";
 
     document.getElementById('one-holder').insertAdjacentHTML('beforeend',
-        "<div class=\"flex-container\" id='" + startLoc + endLoc + "'>" +
+        "<br><br><div class=\"flex-container\" id='" + startLoc + endLoc + "'>" +
         "<div id=\"flex1div\"> <b>From: </b>" + startLoc + "<b> To: </b>" + endLoc +
         "</div><div id=\"flex2div\" style=\"margin-left: 3%;\"><img src=\"../static/img/place.png\" style=\"height: 35px; width: 35px\"; value='" + startLoc + "|" + endLoc + "' id=\"planJourney\" onclick=\"getFavRoute('" + startLoc + "','" + endLoc + "')\">" + "&nbsp" + "&nbsp" + "&nbsp" +
         "<img src=\"../static/img/like.png\" style=\"height: 35px; width: 35px; id=\"delete\" onclick=\"removeFav('" + startLoc + "','" + endLoc + "')\">" +
